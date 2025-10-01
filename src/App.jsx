f8fb8d835d8fec6ec0757d0200eb540a20fa2e11
Udbhav <udbhav227@gmail.com>
2025-10-01 16:38:24 +0000

    add framer-motion animation

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,74 @@
-import React from 'react';
-import BackgroundVideo from './components/BackgroundVideo';
-import Home from './components/Home';
-import Game from './components/Game';
+import React from "react";
+import { motion, AnimatePresence } from "framer-motion"; // Import motion and AnimatePresence
+import BackgroundVideo from "./components/BackgroundVideo";
+import Home from "./components/Home";
+import Game from "./components/Game";
+
+const pageVariants = {
+  initial: {
+    opacity: 0,
+    y: 20,
+  },
+  in: {
+    opacity: 1,
+    y: 0,
+  },
+  out: {
+    opacity: 0,
+    y: -20,
+  },
+};
+
+const pageTransition = {
+  type: "tween",
+  ease: "anticipate",
+  duration: .5,
+};
 
 function App() {
-  const [gameState, setGameState] = React.useState('home');
+  const [gameState, setGameState] = React.useState("home");
 
   const startGame = () => {
-    console.log("start game fired!")
-    setGameState('playing');
-  }
-  const goHome = () => setGameState('home');
+    setGameState("playing");
+  };
+  const goHome = () => setGameState("home");
 
   return (
     <>
       <BackgroundVideo />
       <div className="app-container">
-        { gameState === 'home' && <Home onStartGame={startGame} /> }
-        { gameState === 'playing' && <Game onGoHome={goHome}/> }
+        <main>
+          <AnimatePresence mode="wait">
+            {gameState === "home" && (
+              <motion.div
+                key="home"
+                initial="initial"
+                animate="in"
+                exit="out"
+                variants={pageVariants}
+                transition={pageTransition}
+              >
+                <Home onStartGame={startGame} />
+              </motion.div>
+            )}
+
+            {gameState === "playing" && (
+              <motion.div
+                key="playing"
+                initial="initial"
+                animate="in"
+                exit="out"
+                variants={pageVariants}
+                transition={pageTransition}
+              >
+                <Game onGoHome={goHome} />
+              </motion.div>
+            )}
+          </AnimatePresence>
+        </main>
       </div>
     </>
-  )
+  );
 }
 
 export default App;
