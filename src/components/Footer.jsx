1a3bfdaaff7a82fe6202f301829e105f94896d02
Udbhav <udbhav227@gmail.com>
2025-10-04 08:07:55 +0000

    Add bgm

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,14 +1,43 @@
-import React, { useState } from "react";
+import React from "react";
 import "../styles/Footer.css";
 
 import MusicOnIconPath from "../assets/icons/music-note-svgrepo-com.svg";
 import MusicOffIconPath from "../assets/icons/music-note-slash-svgrepo-com.svg";
 import SfxOnIconPath from "../assets/icons/sound-on-svgrepo-com.svg";
 import SfxOffIconPath from "../assets/icons/sound-off-svgrepo-com.svg";
+import bgm from "../assets/sounds/Pokémon Opening - Gotta Catch 'Em All (Lofi Version).mp3";
 
 const Footer = () => {
-  const [isMusicOn, setIsMusicOn] = useState(true);
-  const [isSfxOn, setIsSfxOn] = useState(true);
+  const [isMusicOn, setIsMusicOn] = React.useState(false);
+  const [isSfxOn, setIsSfxOn] = React.useState(true);
+
+  const audioRef = React.useRef(null);
+
+  React.useEffect(() => {
+    if (!audioRef.current) {
+      audioRef.current = new Audio(bgm);
+      audioRef.current.loop = true;
+      audioRef.current.volume = 0.5;
+    }
+  }, []);
+
+  React.useEffect(() => {
+    if (audioRef.current) {
+      if (isMusicOn) {
+        audioRef.current
+          .play()
+          .catch((error) => console.error("Audio play failed:", error));
+      } else {
+        audioRef.current.pause();
+      }
+    }
+
+    return () => {
+      if (audioRef.current) {
+        audioRef.current.pause();
+      }
+    };
+  }, [isMusicOn]);
 
   const handleMusic = () => setIsMusicOn((prev) => !prev);
   const handleSfx = () => setIsSfxOn((prev) => !prev);
@@ -32,7 +61,7 @@ const Footer = () => {
             />
           </span>
         </button>
-        
+
         <button
           className={`footer-icon-button ${isMusicOn ? "active" : ""}`}
           onClick={handleMusic}
