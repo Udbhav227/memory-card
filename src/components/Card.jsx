cc6bad0cf43e7d0aa1a2684209be6ac96ec8080f
Udbhav <udbhav227@gmail.com>
2025-10-05 09:21:31 +0000

    Refactor Card component: update props for triggering flip effect and enhance rendering structure

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,31 +1,33 @@
+import React from "react";
 import Tilt from "react-parallax-tilt";
+import "../styles/Card.css";
+import cardBackImage from "../assets/img/card_background.jpg";
 
-const Card = ({ character, isFlipped, handleCardClick }) => {
+const Card = ({ character, triggerFlip }) => {
   return (
-    <div
-      className={isFlipped ? "card flipped" : "card"}
-      onClick={() => {
-        handleCardClick(character);
-      }}
+    <Tilt
+      glareEnable={true}
+      glareMaxOpacity={0.6}
+      glareColor="gold"
+      glarePosition="bottom"
+      scale={1.05}
+      transitionSpeed={1500}
+      tiltMaxAngleX={15}
+      tiltMaxAngleY={15}
+      className={triggerFlip ? "flip-card flip-card--flip" : "flip-card"}
     >
-      <Tilt
-        glareEnable={true}
-        glareMaxOpacity={0.6}
-        glareColor="#ffffff"
-        glarePosition="bottom"
-        glareBorderRadius="20px"
-        className="tilt"
-      >
-        <div className="cardFace">
-          <div
-            className="characterHolder"
-            style={{ backgroundImage: `url(${character.src})` }}
-          />
-          <div className="name">{character.name}</div>
+      <div className="flip-card-inner">
+        <div className="flip-card-front">
+          <img src={character.src} alt={character.name} />
+          <div className="card-name-container">
+            <p>{character.name}</p>
+          </div>
         </div>
-        <div className="cardBack"></div>
-      </Tilt>
-    </div>
+        <div className="flip-card-back">
+          <img src={cardBackImage} alt="card back" />
+        </div>
+      </div>
+    </Tilt>
   );
 };
 
