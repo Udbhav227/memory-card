a5dc0fc7dd3912b0f2e0a0b298a3080bd5d06991
Udbhav <udbhav227@gmail.com>
2025-10-05 18:54:48 +0000

    Refactor Card component: improve structure and styling, adjust gap in layout, and enhance user experience with updated background color and pointer events

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,41 +1,38 @@
-import React from 'react';
-import Tilt from 'react-parallax-tilt';
-import '../styles/Card.css';
-import cardBackImage from '../assets/img/card_background.jpg';
+import React from "react";
+import Tilt from "react-parallax-tilt";
+import "../styles/Card.css";
+import cardBackImage from "../assets/img/card_background.jpg";
 
 const Card = ({ character, triggerFlip, handleCardClick }) => {
   return (
     <div
-      className={triggerFlip ? 'flip-card flip-card--flip' : 'flip-card'}
+      className={triggerFlip ? "flip-card flip-card--flip" : "flip-card"}
       onClick={() => handleCardClick(character)}
     >
-      <div className="flip-card-inner">
-        <div className="flip-card-front">
-          <Tilt
-            glareEnable={true}
-            glareMaxOpacity={0.6}
-            glareColor="#ffffff"
-            glarePosition="all"
-            scale={1.06}
-            transitionSpeed={1500}
-            tiltMaxAngleX={15}
-            tiltMaxAngleY={15}
-            glareBorderRadius="10px"
-            className="tilt-component"
-          >
-            <img
-              src={character.src}
-              alt={character.name}
-            />
+      <Tilt
+        glareEnable={true}
+        glareMaxOpacity={0.6}
+        glareColor="#ffffff"
+        glarePosition="bottom"
+        scale={1.05}
+        transitionSpeed={1500}
+        tiltMaxAngleX={15}
+        tiltMaxAngleY={15}
+        glareBorderRadius="10px"
+        className="tilt-component"
+      >
+        <div className="flip-card-inner">
+          <div className="flip-card-front">
+            <img src={character.src} alt={character.name} />
             <div className="card-name-container">
               <p>{character.name}</p>
             </div>
-          </Tilt>
+          </div>
+          <div className="flip-card-back">
+            <img src={cardBackImage} alt="card back" />
+          </div>
         </div>
-        <div className="flip-card-back">
-          <img src={cardBackImage} alt="card back" />
-        </div>
-      </div>
+      </Tilt>
     </div>
   );
 };
