cda8ef235b6a6f40cc04a1530bace4708910d3d4
Udbhav <udbhav227@gmail.com>
2025-10-05 17:37:35 +0000

    Refactor Characters and gameMode components: replace dynamic IDs with static values in Characters, and rename cardsPerRound to cards in gameMode for consistency

--- src/gameMode.js
+++ src/gameMode.js
@@ -1,15 +1,15 @@
 const gameModes = {
   easy: {
     rounds: 5,
-    cardsPerRound: 3,
+    cards: 3,
   },
   medium: {
     rounds: 7,
-    cardsPerRound: 4,
+    cards: 4,
   },
   hard: {
     rounds: 10,
-    cardsPerRound: 5,
+    cards: 5,
   },
 };
 
