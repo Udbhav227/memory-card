156162eb18dc4249c7ec920410359037ffc483c3
Udbhav <udbhav227@gmail.com>
2025-10-06 14:05:09 +0000

    Add fallback background image for video component

--- src/components/BackgroundVideo.jsx
+++ src/components/BackgroundVideo.jsx
@@ -1,9 +1,18 @@
 import "../styles/BackgroundVideo.css";
 import videoSrc from "../assets/videos/bg.mp4";
+import posterImage from "../assets/img/fallback-bg.png";
 
 const BackgroundVideo = () => {
   return (
-    <video autoPlay loop muted playsInline src={videoSrc} className="bg-vid" />
+    <video 
+      autoPlay 
+      loop 
+      muted 
+      playsInline 
+      src={videoSrc} 
+      poster={posterImage}
+      className="bg-vid" 
+    />
   );
 };
 
